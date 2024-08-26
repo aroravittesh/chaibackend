b62231d2217039613c930466eb6582def213561b
aroravittesh <vittesharora04@gmail.com>
2024-08-26 14:53:29 +0530

    github data change

--- index.js
+++ index.js
@@ -4,6 +4,43 @@ const app = express()
 
 const port = 4000
 
+const githubData=
+    {
+        "login": "aroravittesh",
+        "id": 120556714,
+        "node_id": "U_kgDOBy-Mqg",
+        "avatar_url": "https://avatars.githubusercontent.com/u/120556714?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/aroravittesh",
+        "html_url": "https://github.com/aroravittesh",
+        "followers_url": "https://api.github.com/users/aroravittesh/followers",
+        "following_url": "https://api.github.com/users/aroravittesh/following{/other_user}",
+        "gists_url": "https://api.github.com/users/aroravittesh/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/aroravittesh/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/aroravittesh/subscriptions",
+        "organizations_url": "https://api.github.com/users/aroravittesh/orgs",
+        "repos_url": "https://api.github.com/users/aroravittesh/repos",
+        "events_url": "https://api.github.com/users/aroravittesh/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/aroravittesh/received_events",
+        "type": "User",
+        "site_admin": false,
+        "name": null,
+        "company": null,
+        "blog": "",
+        "location": null,
+        "email": null,
+        "hireable": null,
+        "bio": null,
+        "twitter_username": null,
+        "public_repos": 9,
+        "public_gists": 0,
+        "followers": 0,
+        "following": 0,
+        "created_at": "2022-12-14T08:41:47Z",
+        "updated_at": "2024-08-26T08:45:45Z"
+      }
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -18,6 +55,10 @@ app.get('/login', (req,res) => {
 
 app.get('/youtube', (req,res) => {
     res.send('<h2>Chai aur code </h2>')
+})
+
+app.get('/github', (req,res) => {
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
